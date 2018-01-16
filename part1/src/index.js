adc551abd6fb1cba805612480bad7bc127fe418b
Sasu Mäkinen <sasu.makinen@helsinki.fi>
2018-01-16 10:59:54 +0200

    checkpoint for exercise 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 const Title = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -13,10 +13,10 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part name={props.parts[0].name} amount={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} amount={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} amount={props.parts[2].exercises} />
-    </div>
+      <Part name={props.course.parts[0].name} amount={props.course.parts[0].exercises} />
+      <Part name={props.course.parts[1].name} amount={props.course.parts[1].exercises} />
+      <Part name={props.course.parts[2].name} amount={props.course.parts[2].exercises} />
+      </div>
   )
 }
 const Part = (props) => {
@@ -28,34 +28,37 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const sum = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
   return (
     <div>
-      <p>yhteensä {props.total[0].exercises+props.total[1].exercises+props.total[2].exercises} tehtävää </p>
+      <p>yhteensä {sum} tehtävää </p>
     </div>
   )
 }
 const App = () => {
   // const-määrittelyt
-  const course = 'Half Stack -sovelluskehitys'
-  const parts = [
-    {
-      name: 'Reactin perusteet',
-      exercises: 10
-    },
-    {
-      name: 'Tiedonvälitys propseilla',
-      exercises: 7
-    },
-    {
-      name: 'Komponenttien tila',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <Title course={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
