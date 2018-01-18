45a930d5070c3e2f9d8bc15c4356c397d7ca7109
Sasu Mäkinen <sasu.makinen@helsinki.fi>
2018-01-18 13:00:27 +0200

    phonebook reads data from json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import Person from "./components/Person";
+import axios from "axios";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      persons: [
-        { name: "Arto Hellas", number: "040-123456" },
-        { name: "Martti Tienari", number: "040-123456" },
-        { name: "Arto Järvinen", number: "040-123456" },
-        { name: "Lea Kutvonen", number: "040-123456" }
-      ],
+      persons: [],
       newName: "",
       newNumber: "",
       filter: ""
     };
+  }
+  componentWillMount() {
+    console.log("Getting data from http://localhost:3001/persons");
+    axios.get("http://localhost:3001/persons").then(response => {
+      console.log("promise fulfilled");
+      this.setState({ persons: response.data });
+    });
   }
   handleNameChange = event => {
     this.setState({ newName: event.target.value });
